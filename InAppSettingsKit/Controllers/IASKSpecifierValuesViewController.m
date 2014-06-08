c53885392c6a3539df54a3b627eb3e679ee87494
Guy Shaviv <guy@tivstudio.com>
2014-06-08 22:28:33 +0300

    xcode6 fixes

--- InAppSettingsKit/Controllers/IASKSpecifierValuesViewController.m
+++ InAppSettingsKit/Controllers/IASKSpecifierValuesViewController.m
@@ -104,12 +104,6 @@
 	// Release any cached data, images, etc that aren't in use.
 }
 
-- (void)viewDidUnload {
-	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
-	self.tableView = nil;
-}
-
 #pragma mark -
 #pragma mark UITableView delegates
 
